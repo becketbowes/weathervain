b86816150b63e80acbf4a10e9ca94582e47f2ac7
becket <becket.bowes@gmail.com>
2022-01-22 20:26:52 -0500

    basic redesign

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var placeSearchName = document.querySelector("textarea").textContent;
+
+// var whereInTheWorld = function {
+
+//     varGoogleLiteralSearch = https://maps.googleapis.com/maps/api/geocode/json?
+// }
+
+// var lookOutTheWindow = function() {
+
+// }
+
+addEventListener("click", function() {
+    console.log(placeSearchName);
+});
+
+// whereInTheWorld(text))
+// google locator api key : AIzaSyDWtVKZCyc6X5L_eERu0Bk_WpclnefusjU
+
+
+//design layout
+//create search field
+//return seach to local storage/ new div
+//make saved divs active searches
+//script req to server
+//return current weather to main field
+//weather, temp, humidity, windspeed, UV index
+//color code UV
+//return five day forcast to cards
+//add icons to forcast
