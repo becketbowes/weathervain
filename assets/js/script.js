dd644ded4c722b4e10e62c8b6d34f2d303c6a5c1
becket <becket.bowes@gmail.com>
2022-01-25 16:16:35 -0500

    returning now results to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,68 @@
+// //make weather data variables global
+// var nowWeather = "";
+// var nowTemp = "";
+// var nowTempFeels = "";
+// var nowHumidity = "";
+// var nowWind = "";
+// var nowUv = "";
+// var nowWeatherCode = "";
+// var weatherHeadline = "";
+// var weatherLede = "";
+// //Today's Weather report    
+// var today = "";
+// var todayHigh = "";
+// var todayLow = "";
+// var todayWind = "";
+// var todayHumid = "";
+// var todayWeather = "";
+// var todayReport = "";
+// //5 day forecast
+// //dayplusone    
+// var dayOne = "";
+// var dayOneHigh = "";
+// var dayOneLow = "";
+// var dayOneWind = "";
+// var dayOneHumid = "";
+// var dayOneWeather = "";
+// var dayOneReport = "";
+// var dayOneIcon = "";
+// //dayplustwo    
+// var dayTwo = "";
+// var dayTwoHigh = "";
+// var dayTwoLow = "";
+// var dayTwoWind = "";
+// var dayTwoHumid = "";
+// var dayTwoWeather = "";
+// var dayTwoReport = "";
+// var dayTwoIcon = "";
+// //dayplusthree    
+// var dayThree = "";
+// var dayThreeHigh = "";
+// var dayThreeLow = "";
+// var dayThreeWind = "";
+// var dayThreeHumid = "";
+// var dayThreeWeather = "";
+// var dayThreeReport = "";
+// var dayThreeIcon = "";
+// //dayplusfour    
+// var dayFour = "";
+// var dayFourHigh = "";
+// var dayFourLow = "";
+// var dayFourWind = "";
+// var dayFourHumid = "";
+// var dayFourWeather = "";
+// var dayFourReport = "";
+// var dayFourIcon = "";
+// //dayplusfive
+// var dayFive = "";
+// var dayFiveHigh = "";
+// var dayFiveLow = "";
+// var dayFiveWind = "";
+// var dayFiveHumid = "";
+// var dayFiveWeather = "";
+// var dayFiveReport = "";
+// var dayFiveIcon = "";
+
 //get latitude and longitude from user request
 var whereInTheWorld = function(param) {
     var theAsk = ("https://maps.googleapis.com/maps/api/geocode/json?address=" + param + "&key=AIzaSyDWtVKZCyc6X5L_eERu0Bk_WpclnefusjU")
@@ -46,7 +111,7 @@ var howsTheWeather = function(townState, lat, lng) {
         var nowUv = (data.current.uvi);
         var nowWeatherCode = (data.current.weather[0].icon);
         var weatherHeadline = ("It's " + nowWeather + " in " + townState);
-        var weatherLede = ("The temp is " + nowTemp + " farenheight, but it definitely feels like " + nowTempFeels + ". The humidity is only " + nowHumidity + ", and the wind is blowing at a mild " + nowWind + ". You should definitely moisturize. Not that you need it. Also, the UV is " + nowUv + ", which feels like this color: ");
+        var weatherLede = ("The temp is " + nowTemp + " farenheight, but it definitely feels like " + nowTempFeels + ". The humidity is, like, " + nowHumidity + "%, and the wind is blowing at a mild and/or whopping " + nowWind + "mph. You should definitely moisturize, though anyhow. Not that you need it. Also, babe, just for reference: moderate UV indexes are, like, 3-5. Right now the UV is " + nowUv + ", which in terms of, say, stop lights, feels like this color: ");
 
         // date fixing function
             var fixDate = function(day) {
@@ -65,8 +130,8 @@ var howsTheWeather = function(townState, lat, lng) {
         var todayWind = (data.daily[0].wind_speed);
         var todayHumid = (data.daily[0].humidity);
         var todayWeather = (data.daily[0].weather[0].main);
-        var todayReport = (today + "<br/>" + todayWeather + " today, with a high of " + todayHigh + ", and a low of " + todayLow + ". The wind will be around " + todayWind + "mph, and humidity will be around " + todayHumid + "%. But we all know you'll bring your own weather anyway;)") 
-        var todayIcon = (data.daily[0].weather[0].icon);
+        var todayReport = (todayWeather + " in general today, with a high of " + todayHigh + ", and a low of " + todayLow + ". The wind will be around " + todayWind + "mph, and humidity will be around " + todayHumid + "%. Dress like you own it, because you DO!"); 
+        console.log(todayReport);
 
         //5 day forecast
         //dayplusone    
@@ -76,7 +141,7 @@ var howsTheWeather = function(townState, lat, lng) {
         var dayOneWind = (data.daily[1].wind_speed);
         var dayOneHumid = (data.daily[1].humidity);
         var dayOneWeather = (data.daily[1].weather[0].main);
-        var dayOneReport = (dayOne + "<br/>" + dayOneWeather + " today, with a high of " + dayOneHigh + ", and a low of " + dayOneLow + ". The wind will be around " + dayOneWind + "mph, and humidity will be around " + dayOneHumid + "%. But we all know you'll bring your own weather anyway;)") 
+        var dayOneReport = (dayOne + "<br/>" + dayOneWeather + " today, with a high of " + dayOneHigh + ", and a low of " + dayOneLow + ". The wind will be around " + dayOneWind + "mph, and humidity will be around " + dayOneHumid + "%. You know you're aging like fine wine, babe. Tomorrow you just be that much better. "); 
         var dayOneIcon = (data.daily[1].weather[0].icon);
         
         //dayplustwo    
@@ -86,7 +151,7 @@ var howsTheWeather = function(townState, lat, lng) {
         var dayTwoWind = (data.daily[2].wind_speed);
         var dayTwoHumid = (data.daily[2].humidity);
         var dayTwoWeather = (data.daily[2].weather[0].main);
-        var dayTwoReport = (dayTwo + "<br/>" + dayTwoWeather + " today, with a high of " + dayTwoHigh + ", and a low of " + dayTwoLow + ". The wind will be around " + dayTwoWind + "mph, and humidity will be around " + dayTwoHumid + "%. But we all know you'll bring your own weather anyway;)") 
+        var dayTwoReport = (dayTwo + "<br/>" + dayTwoWeather + " today, with a high of " + dayTwoHigh + ", and a low of " + dayTwoLow + ". The wind will be around " + dayTwoWind + "mph, and humidity will be around " + dayTwoHumid + "%. I'd say that this weather is the most flattering for you, but holy hell, look at you Right Now. In this weather! Killing it."); 
         var dayTwoIcon = (data.daily[2].weather[0].icon);
 
         //dayplusthree    
@@ -96,7 +161,7 @@ var howsTheWeather = function(townState, lat, lng) {
         var dayThreeWind = (data.daily[3].wind_speed);
         var dayThreeHumid = (data.daily[3].humidity);
         var dayThreeWeather = (data.daily[3].weather[0].main);
-        var dayThreeReport = (dayThree + "<br/>" + dayThreeWeather + " today, with a high of " + dayThreeHigh + ", and a low of " + dayThreeLow + ". The wind will be around " + dayThreeWind + "mph, and humidity will be around " + dayThreeHumid + "%. But we all know you'll bring your own weather anyway;)") 
+        var dayThreeReport = (dayThree + "<br/>" + dayThreeWeather + " today, with a high of " + dayThreeHigh + ", and a low of " + dayThreeLow + ". The wind will be around " + dayThreeWind + "mph, and humidity will be around " + dayThreeHumid + "%. Make some plans around it babe, because you'll feel that much more glam when you cancel."); 
         var dayThreeIcon = (data.daily[3].weather[0].icon);
 
         //dayplusfour    
@@ -106,7 +171,7 @@ var howsTheWeather = function(townState, lat, lng) {
         var dayFourWind = (data.daily[4].wind_speed);
         var dayFourHumid = (data.daily[4].humidity);
         var dayFourWeather = (data.daily[4].weather[0].main);
-        var dayFourReport = (dayFour + "<br/>" + dayFourWeather + " today, with a high of " + dayFourHigh + ", and a low of " + dayFourLow + ". The wind will be around " + dayFourWind + "mph, and humidity will be around " + dayFourHumid + "%. But we all know you'll bring your own weather anyway;)") 
+        var dayFourReport = (dayFour + "<br/>" + dayFourWeather + " today, with a high of " + dayFourHigh + ", and a low of " + dayFourLow + ". The wind will be around " + dayFourWind + "mph, and humidity will be around " + dayFourHumid + "%. So, is the question which hat or what hair!?"); 
         var dayFourIcon = (data.daily[4].weather[0].icon);
 
         //dayplusfive
@@ -116,32 +181,44 @@ var howsTheWeather = function(townState, lat, lng) {
         var dayFiveWind = (data.daily[5].wind_speed);
         var dayFiveHumid = (data.daily[5].humidity);
         var dayFiveWeather = (data.daily[5].weather[0].main);
-        var dayFiveReport = (dayFive + "<br/>" + dayFiveWeather + " today, with a high of " + dayFiveHigh + ", and a low of " + dayFiveLow + ". The wind will be around " + dayFiveWind + "mph, and humidity will be around " + dayFiveHumid + "%. But we all know you'll bring your own weather anyway;)") 
+        var dayFiveReport = (dayFive + "<br/>" + dayFiveWeather + " today, with a high of " + dayFiveHigh + ", and a low of " + dayFiveLow + ". The wind will be around " + dayFiveWind + "mph, and humidity will be around " + dayFiveHumid + "%. Nobody really knows what the weather is like five days out, but don't worry, babe, you make your own weather anyway;)"); 
         var dayFiveIcon = (data.daily[5].weather[0].icon);
 
         //console.log the values to return:
-        console.log(todayReport, todayIcon);
         console.log(dayOneReport, dayOneIcon);
         console.log(dayTwoReport, dayTwoIcon);
         console.log(dayThreeReport, dayThreeIcon);
         console.log(dayFourReport, dayFourIcon);
         console.log(dayFiveReport, dayFiveIcon);
-        console.log(weatherHeadline);
-        console.log(weatherLede);
-        console.log(nowWeatherCode);
-        // color code uv index 
-        console.log(nowUv);
+
+        nowCast(weatherHeadline, weatherLede, today, todayReport, nowWeatherCode, nowUv)
     });
 };
 
-// codes for weather:
-// 200 = thunderstorm
-// 300 = drizzle
-// 500 = rain
-// 600 = snow
-// 700 = atmosphere
-// 800 - 804 clear to cloudy spectrum
-
+//diplay current weather
+var nowCast = function(weatherHeadline, weatherLede, today, todayReport, nowWeatherCode, nowUv) {
+        var headline = document.getElementById("headline");
+        var lede = document.getElementById("lede");
+        var uv = document.getElementById("uv");
+        var reportDay = document.getElementById("report-day");
+        var date = document.getElementById("date");
+        var report = document.getElementById("report");
+        uv.classList.remove("hide");
+        reportDay.classList.remove("hide");
+        headline.textContent = weatherHeadline;
+        lede.textContent = weatherLede;
+        date.textContent = ("Today, " + today);
+        report.textContent = todayReport;
+        //color code Uv
+        if (nowUv>6) {
+            uv.setAttribute("style", "background-color:red");
+        } else if (nowUv <= 6 && nowUv >= 3) {
+            uv.setAttribute("style", "background-color:yellow");
+        } else {
+            uv.setAttribute("style", "background-color:green");
+        };
+        console.log(nowWeatherCode);
+}
 
 //make buttons out of user requests
 var placeButton = function (townState, lat, lng) {
@@ -184,23 +261,3 @@ document.getElementById("btn").addEventListener("click", function () {
     };
     clearText();
 });
-
-
-// var lookOutTheWindow = function() {
-
-// }
-
-// whereInTheWorld(text))
-// google locator api key : AIzaSyDWtVKZCyc6X5L_eERu0Bk_WpclnefusjU
-
-
-//design layout
-//create search field
-//return seach to local storage/ new div
-//make saved divs active searches
-//script req to server
-//return current weather to main field
-//weather, temp, humidity, windspeed, UV index
-//color code UV
-//return five day forcast to cards
-//add icons to forcast
